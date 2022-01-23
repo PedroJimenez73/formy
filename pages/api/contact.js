99b99b05f3c98e1c3089b18bcf20f5b79b443c3a
PedroJimenez73 <pjimenez@qualitycw.com>
2022-01-23 11:11:58 +0100

    new promise

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -54,13 +54,23 @@ export default async (req, res) => {
         html: message.replace(/\r\n/g, '<br />'),
     }
     await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+    await new Promise((resolve, reject) => {
         transporter.sendMail(mailData, (err, info) => {
             if (err) {
-                console.error(err);
                 reject(err);
                 res.status(500).json({ status: 'ERROR' });
             } else {
-                console.log(info);
                 resolve(info);
                 res.status(200).json({ status: 'OK' });
             }
