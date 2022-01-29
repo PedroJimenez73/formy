380d26bf5e03cc40de54af3f5ac5a827fa30ca63
PedroJimenez73 <pjimenez@qualitycw.com>
2022-01-29 12:17:20 +0100

    add link

--- pages/components/ContactForm.js
+++ pages/components/ContactForm.js
@@ -75,7 +75,7 @@ export default function ContactForm() {
                     <div>
                         <input onClick={() => handleAccept()} type="checkbox" id="accept" />
                         <label htmlFor="accept">
-                            He leído y acepto la <a href="http://isca.itweblearning.com/aviso-legal" target="_blank"></a> política de privacidad de este sitio web
+                            He leído y acepto la <a href="http://isca.itweblearning.com/aviso-legal" target="_blank">política de privacidad</a>  de este sitio web
                         </label>
                     </div>
                     <button type="submit">Enviar</button>
